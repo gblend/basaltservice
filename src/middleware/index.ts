edcebd445e0c03d98e0846172cb2b6ef7d85ff29
Gabriel Ilochi <gabrielilo190@gmail.com>
2024-01-20 08:39:12 +0100

    export middleware modules

---
+++ src/middleware/index.ts
@@ -0,0 +1,9 @@
+export { authenticateUser, authorizeRoles } from './authentication';
+
+export { errorHandler } from './error_handler';
+
+export { eventHandler } from './event_handler';
+
+export { routeNotFound } from './undefined_route';
+
+export { responseInterceptor } from './response_interceptor';
